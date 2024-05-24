a9ee3552702dc57bbb3ac157ca7a97d4f020441c
Sonia <161872642+Inflanubes@users.noreply.github.com>
2024-05-24 20:00:20 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React,{useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [shadowDanger, setShadowDanger] = useState("shadow-lg");
+	const [shadowWarning, setShadowWarning] = useState("shadow-lg");
+	const [shadowGreen, setShadowGreen] = useState("shadow-lg");
+
+	function activeShadowDanger() {
+		console.log("entramos en on Click")
+		setShadowDanger("shadow-lg")
+		setShadowWarning("")
+		setShadowGreen("")
+	};
+
+	function activeShadowWarning() {
+		console.log("entramos en on Click")
+
+		setShadowDanger("")
+		setShadowWarning("shadow-lg")
+		setShadowGreen("")
+	};
+
+	function activeShadowGreen() {
+		console.log("entramos en on Click")
+
+		setShadowDanger("")
+		setShadowWarning("")
+		setShadowGreen("shadow-lg")
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="trafficLight d-flex row justify-content-center ">
+			<button className={`btn btn-danger rounded-circle ${shadowDanger}`} onClick={activeShadowDanger}>R</button>
+			<button className={`btn btn-warning rounded-circle ${shadowWarning}`} onClick={activeShadowWarning}>W</button>
+			<button className={`btn btn-success rounded-circle ${shadowGreen}`} onClick={activeShadowGreen}>S</button>
 		</div>
 	);
 };
